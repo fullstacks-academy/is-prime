30e0e8d9314648b1247d18bac1a496350de9c996
frontendmonster <frontendmonster@gmail.com>
2020-09-04 22:22:40 +0430

    fix bug

--- src/isPrime.js
+++ src/isPrime.js
@@ -1,6 +1,6 @@
 function isPrime(n) {
-  for(var i = 0; i < n; i++) {
-    if(n%i===0) return false;
+  for (var i = 1; i < n; i++) {
+    if (n % i === 0) return false;
   }
-  return true
+  return true;
 }
