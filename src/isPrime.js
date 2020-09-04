f4ae43bfd845a2c887653d3a2d39c89d6d49bfb3
frontendmonster <frontendmonster@gmail.com>
2020-09-04 23:05:01 +0430

    fix increate performance

--- src/isPrime.js
+++ src/isPrime.js
@@ -1,7 +1,7 @@
 function isPrime(n) {
   if (n < 2) return false;
 
-  for (let i = 2; i < n; i++) {
+  for (let i = 2; i<Math.sqrt(n); i++) {
     if (n % i === 0) return false;
   }
   return true;
